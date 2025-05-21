e4cd961f323105be46bfdbb64c36fca60d3eb317
KC111189 <casey.nketia.cn@gmail.com>
2025-05-21 15:32:27 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_5MQfuI01N', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '42c52i8jbeke3s2jhcl67gq8ol', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
