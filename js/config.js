a8b1c802dc79e13a0bbfeeb8b446b3446135327a
KC111189 <casey.nketia.cn@gmail.com>
2025-05-21 15:56:47 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_5MQfuI01N', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '42c52i8jbeke3s2jhcl67gq8ol', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_32QSBqqTf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2astu08r821g1q7ard7dt0gb3l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
